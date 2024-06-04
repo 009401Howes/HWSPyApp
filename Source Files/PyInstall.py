dd51f081ebae5f256b758fd66e102d0b37edcbe7
009401Howes <107249983+009401Howes@users.noreply.github.com>
2024-06-04 13:05:05 +0100

    Update PyInstall.py

--- Source Files/PyInstall.py
+++ Source Files/PyInstall.py
@@ -1,32 +1,12 @@
-From: <Saved by Blink>
-Snapshot-Content-Location: https://publicassets.w3spaces.com/PyInstall.html
-Subject: 
-Date: Tue, 4 Jun 2024 12:56:13 +0100
-MIME-Version: 1.0
-Content-Type: multipart/related;
-	type="text/html";
-	boundary="----MultipartBoundary--CGC2Bxy0ar7ZUFs6XYpvScI875bSpDmfpJHAqTuVQE----"
-
-
-------MultipartBoundary--CGC2Bxy0ar7ZUFs6XYpvScI875bSpDmfpJHAqTuVQE----
-Content-Type: text/html
-Content-ID: <frame-8BE131F0E81A86BBC5A636AC42FDE4FA@mhtml.blink>
-Content-Transfer-Encoding: quoted-printable
-Content-Location: https://publicassets.w3spaces.com/PyInstall.html
-
-<html><head><meta http-equiv=3D"Content-Type" content=3D"text/html; charset=
-=3Dwindows-1252"></head><body>import os
+import os
 os.system("pip install wget")
 import wget
-StartPath =3D os.getcwd()
+StartPath = os.getcwd()
 os.chdir("C:\\")
 os.mkdir("py")
 os.chdir("C:\\py\\")
-wget.download("https://publicassets.w3spaces.com/PySimpleGUI.html", "PySimp=
-leGUI.py")
+wget.download("https://publicassets.w3spaces.com/PySimpleGUI.html", "PySimpleGUI.py")
 wget.download("https://publicassets.w3spaces.com/py.html", "py.py")
 os.chdir(StartPath)
-wget.download("https://publicassets.w3spaces.com/PyStart.html", "PyStart.py=
-")
-exit</body></html>
-------MultipartBoundary--CGC2Bxy0ar7ZUFs6XYpvScI875bSpDmfpJHAqTuVQE------
+wget.download("https://publicassets.w3spaces.com/PyStart.html", "PyStart.py")
+exit
